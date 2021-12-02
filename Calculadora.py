394c6df5662ae930e834af19701326e42b5096b6
Estudo <breno.eustaq@gmail.com>
2021-12-02 00:18:15 -0300

    - Primeiro testes com código

---
+++ Calculadora.py
@@ -0,0 +1,63 @@
+
+class Calculadora:
+    def __init__(self):
+        self.opcoes
+
+    def opcoes(self):
+        print("*** Teste pra calculadora ***")
+        print("(1) soma \n"
+              "(2) subtração \n"
+              "(3) multiplicação \n"
+              "(4) divisão \n")
+        operacao = int(input("Qual operação deve ser realizada? "))
+        operacoes = [1,2,3,4]
+        while operacao != operacoes:
+            print("Insira um valor válido.")
+            operacao = int(input("Qual operação deve ser realizada? "))
+
+
+        if operacao == 1:
+            primeiro_valor = input("Insira o primeiro valor: ")
+            segundo_valor = input("Insira o segundo valor : ")
+
+            print(self.soma(primeiro_valor, segundo_valor))
+            self.opcoes()
+
+        if operacao == 2:
+            primeiro_valor = input(" Insira o primeiro valor: ")
+            segundo_valor =  input("Insira o segundo valor : ")
+
+            print(self.subtracao(primeiro_valor, segundo_valor))
+            self.opcoes()
+
+        if operacao == 3:
+            primeiro_valor = input(' Insira o primeiro valor: ')
+            segundo_valor = input("Insira o segundo valor : ")
+
+            print((self.multiplicacao(primeiro_valor, segundo_valor)))
+            self.opcoes()
+
+        if operacao == 4:
+            primeiro_valor = input(' Insira o primeiro valor: ')
+            segundo_valor = input("Insira o segundo valor : ")
+
+            print((self.divisao(primeiro_valor, segundo_valor)))
+            self.opcoes()
+
+
+def soma(self, valor1, valor2):
+    self.resultado = valor1 + valor2
+    return f'Resultado = {self.resultado} '
+
+def subtracao(self, valor1, valor2):
+    self.resultado = valor1 - valor2
+    return f'Resultado = {self.resultado}'
+
+def multiplicacao(self, valor1, valor2):
+    self.resultado = valor1 * valor2
+    return f'Resultado = {self.resultado}'
+
+def divisao(self, valor1, valor2):
+    self.resultado = valor1 / valor2
+    return f'Resultado = {self.resultado}'
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Calculadora-em-Py
+
+Versão base da calculadora em python, apenas com as operações de:
+```
+soma (+)
+subtração (-)
+divisão (/)
+multiplicação (*)
+```
